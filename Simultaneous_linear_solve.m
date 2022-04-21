96b247694e0d132de50964d7f9a879d71b86320a
Timbersaw-wangzw <57893826+Timbersaw-wangzw@users.noreply.github.com>
2022-04-21 16:29:10 +0800

    update to quick summation

--- Simultaneous_linear_solve.m
+++ Simultaneous_linear_solve.m
@@ -4,87 +4,170 @@ function update_grpah= Simultaneous_linear_solve(init_graph)
 edge_num=length(init_graph.edge);
 node_num=length(init_graph.node)-1;
 % A=zeros(4,6*edge_num);
-b=zeros(6*node_num,1);
+j=zeros(6*node_num,1);
 A=zeros(6*node_num,6*node_num);
-anchor_idx=1;
-for i=1:edge_num
-%     corre_num=floor(length(init_graph.edge{i}.pair_points)/100);
-    corre_num=init_graph.edge{i}.pair_points_1.Count;
-    fprintf('edge index:%d, correspondence points number: %d\n',[i,init_graph.edge{i}.pair_points_1.Count]);
-    idx_a=init_graph.edge{i}.idx(1);
-    idx_b=init_graph.edge{i}.idx(2);
-    
-    ax=init_graph.edge{i}.pair_points_1.Location(:,1);
-    ay=init_graph.edge{i}.pair_points_1.Location(:,2);
-    az=init_graph.edge{i}.pair_points_1.Location(:,3);
+A_compare=zeros(6*node_num,6*node_num);
 
-    bx=init_graph.edge{i}.pair_points_2.Location(:,1);
-    by=init_graph.edge{i}.pair_points_2.Location(:,2);
-    bz=init_graph.edge{i}.pair_points_2.Location(:,3);
+anchor_idx=1;
+% 测试快速运算是否正确，设定每个位姿有一个变换
+for v=1:node_num+1
+    if v==anchor_idx
+        vec=zeros(6,1);
+    else
+        vec=rand(6,1);
+    end
+    T=SE3.exp(vec);
+    init_graph.node{v}.T=T;
+end
+for e=3:edge_num
+%     corre_num=floor(length(init_graph.edge{i}.pair_points)/100);
+    corre_num=init_graph.edge{e}.pair_points_1.Count;
+    fprintf('edge index:%d, correspondence points number: %d\n',[e,init_graph.edge{e}.pair_points_1.Count]);
+    idx_i=init_graph.edge{e}.idx(1);
+    idx_j=init_graph.edge{e}.idx(2);
+    Ti=init_graph.node{idx_i}.T;
+    Tj=init_graph.node{idx_j}.T;
+
+
+    xi=init_graph.edge{e}.pair_points_1.Location(:,1);
+    yi=init_graph.edge{e}.pair_points_1.Location(:,2);
+    zi=init_graph.edge{e}.pair_points_1.Location(:,3);
+
+    xj=init_graph.edge{e}.pair_points_2.Location(:,1);
+    yj=init_graph.edge{e}.pair_points_2.Location(:,2);
+    zj=init_graph.edge{e}.pair_points_2.Location(:,3);
 
     temp_points=zeros(6*node_num,1);
     A_temp={};
-    if idx_a~=anchor_idx&&idx_b~=anchor_idx
-        if idx_a>anchor_idx
-            a=idx_a-1;
+    A_temp_compare={};
+    a_temp={};
+    skew_p_temp={};
+    skew_q_temp={};
+    qp_temp={};
+    if idx_i~=anchor_idx&&idx_j~=anchor_idx
+        if idx_i>anchor_idx
+            i=idx_i-1;
         end
-        if idx_b>anchor_idx
-            b=idx_b-1;
+        if idx_j>anchor_idx
+            j=idx_j-1;
         end
-        parfor j=1:corre_num
-            Aj=zeros(4,6*node_num);
-            va=[ax(j);ay(j);az(j);1];
-            vb=[bx(j);by(j);bz(j);1];
-            Ma=dotVec(va);
-            Mb=-1*dotVec(vb);
-            Aj(:,(a-1)*6+1:a*6)=Ma;
-            Aj(:,(b-1)*6+1:b*6)=Mb;
-            A_temp{j}=Aj'*Aj;
-            temp_points(:,j)=Aj'*(va-vb);
-        end
-    end
-    if idx_a==anchor_idx
-        if idx_b>anchor_idx
-            b=idx_b-1;
-        end
-        parfor j=1:corre_num
-            Aj=zeros(4,6*node_num);
-            va=[ax(j);ay(j);az(j);1];
-            vb=[bx(j);by(j);bz(j);1];
-            Mb=-1*dotVec(vb);
-            Aj(:,(b-1)*6+1:b*6)=Mb;
-            A_temp{j}=Aj'*Aj;
-            temp_points(:,j)=Aj'*(va-vb);
-        end
-    end
-    if idx_b==anchor_idx
-        if idx_a>anchor_idx
-            a=idx_a-1;
-        end
-        parfor j=1:corre_num
-            Aj=zeros(4,6*node_num);
-            va=[ax(j);ay(j);az(j);1];
-            vb=[bx(j);by(j);bz(j);1];
-            Ma=dotVec(va);
-            Aj(:,(a-1)*6+1:a*6)=Ma;
-            A_temp{j}=Aj'*Aj;
-            temp_points(:,j)=Aj'*(va-vb);
+        parfor k=1:corre_num
+            Ak=zeros(4,6*node_num);
+            p0=[xi(k);yi(k);zi(k);1];
+            q0=[xj(k);yj(k);zj(k);1];
+            Mi0=dotVec(p0);
+            Mj0=-1*dotVec(q0);
+            Ak(:,(i-1)*6+1:i*6)=Mi0;
+            Ak(:,(j-1)*6+1:j*6)=Mj0;
+            A_temp{k}=Ak'*Ak;
+            temp_points(:,k)=Ak'*(p0-q0);
+            skew_p_temp{k}=skew(p0(1:3));
+            skew_q_temp{k}=skew(q0(1:3));
+            qp_temp{k}=q0(1:3)*p0(1:3)';
+
+
+            Ak_compare=zeros(4,6*node_num);
+            p=double(Ti)*[xi(k);yi(k);zi(k);1];
+            q=double(Tj)*[xi(k);yi(k);zi(k);1];
+            Mi=dotVec(p);
+            Mj=-1*dotVec(q);
+            Ak_compare(:,(i-1)*6+1:i*6)=Mi;
+            Ak_compare(:,(j-1)*6+1:j*6)=Mj;
+            A_temp_compare{k}=Ak_compare'*Ak_compare;
         end
     end
-    for j=1:corre_num
-        A=A+A_temp{j};
-        b=b-temp_points(:,j);
+    if idx_i==anchor_idx
+        if idx_j>anchor_idx
+            j=idx_j-1;
+        end
+        parfor k=1:corre_num
+            Ak=zeros(4,6*node_num);
+            p0=[xi(k);yi(k);zi(k);1];
+            q0=[xj(k);yj(k);zj(k);1];
+            Mj0=-1*dotVec(q0);
+            Ak(:,(j-1)*6+1:j*6)=Mj0;
+            a_temp{k}=Mj0'*Mj0;
+            A_temp{k}=Ak'*Ak;
+            temp_points(:,k)=Ak'*(p0-q0);
+
+            Ak_compare=zeros(4,6*node_num);
+            q=double(Tj)*[xi(k);yi(k);zi(k);1];
+            Mj=-1*dotVec(q);
+            Ak_compare(:,(j-1)*6+1:j*6)=Mj;
+            A_temp_compare{k}=Ak_compare'*Ak_compare;
+        end
     end
+    if idx_j==anchor_idx
+        if idx_i>anchor_idx
+            i=idx_i-1;
+        end
+        parfor k=1:corre_num
+            Ak=zeros(4,6*node_num);
+            p0=[xi(k);yi(k);zi(k);1];
+            q0=[xj(k);yj(k);zj(k);1];
+            Mi0=dotVec(p0);
+            Ak(:,(i-1)*6+1:i*6)=Mi0;
+            A_temp{k}=Ak'*Ak;
+            temp_points(:,k)=Ak'*(p0-q0);
+
+            Ak_compare=zeros(4,6*node_num);
+            p=double(Ti)*[xi(k);yi(k);zi(k);1];
+            q=double(Tj)*[xi(k);yi(k);zi(k);1];
+            Mi=dotVec(p);
+            Ak_compare(:,(i-1)*6+1:i*6)=Mi;
+            A_temp_compare{k}=Ak_compare'*Ak_compare;
+        end
+    end
+    a=zeros(6,6);
+    sum_skew_p=zeros(3,3);
+    sum_skew_q=zeros(3,3);
+    sum_qp=zeros(3,3);
+    for k=1:corre_num
+        A=A+A_temp{k};
+        j=j-temp_points(:,k);
+%         a=a+a_temp{k};
+        sum_skew_p=sum_skew_p+skew_p_temp{k};
+        sum_skew_q=sum_skew_q+skew_q_temp{k};
+        sum_qp=sum_qp+qp_temp{k};
+        A_compare=A_compare+A_temp_compare{k};
+    end
+    M=getDiagnoalBlock(Tj,A(1:6,1:6));
+    M1=getNonDiagnoalBlock(sum_skew_p,sum_skew_q,sum_qp,Ti,Tj,corre_num);
 end
-lambda=5*diag(ones(6*node_num,1));
-x=(A+lambda)\b;
+% lambda=5*diag(ones(6*node_num,1));
+x=A\j;
+for e=1:node_num
+    T=SE3.exp(x((e-1)*6+1:6*e));
+    rot=double(SO3(T));
+    trans=double(transl(T));
+    tfrom=rigid3d(rot,trans);
 end
+end
+function M=getDiagnoalBlock(T,sum_m)
+inv_Ad_T=Ad(T)^-1;
+M=inv_Ad_T'*sum_m*inv_Ad_T;
+end
+function M=getNonDiagnoalBlock(sum_skew_p,sum_skew_q,sum_qp,Ti,Tj,N)
+rot1=double(SO3(Ti));
+rot2=double(SO3(Tj));
+W=rot1'*rot2;
+M=zeros(6,6);
+M(1:3,1:3)=-1*W*N;
+M(1:3,4:6)=W*sum_skew_q;
+M(4:6,1:3)=-1*W*sum_skew_p;
+M(4:6,4:6)=-1*(-1*trace(sum_qp)*eye(3)+sum_qp)*(-1*trace(W)*eye(3)+W')+trace(W'*sum_qp)*eye(3)-W'*sum_qp;
+inv_Ad_Ti=Ad(Ti)^-1;
+inv_Ad_Tj=Ad(Tj)^-1;
+M=inv_Ad_Ti'*M*inv_Ad_Tj;
+end
+
+
 function [L,D,U]=LDU(pair_points)
 corre_num=length(pair_points);
 N=floor(corre_num/2000);
 sum_x=[0;0;0];
 sum_y=[0;0;0];
-sum_ma=zeros(6,6);
+sum_Mi=zeros(6,6);
 sum_mb=zeros(6,6);
 ax=pair_points(1,:);
 ay=pair_points(2,:);
@@ -97,9 +180,9 @@ A=zeros(12,12);
 for j=1:N
     va=[ax(j);ay(j);az(j);1];
     vb=[bx(j);by(j);bz(j);1];
-    Ma=dotVec(va);
+    Mi=dotVec(va);
     Mb=-1*dotVec(vb);
-    Aj(:,1:6)=Ma;
+    Aj(:,1:6)=Mi;
     Aj(:,7:12)=Mb;
     A=A+Aj'*Aj;
     sum_x=sum_x+va(1:3);
--- main.m
+++ main.m
@@ -2,10 +2,27 @@ clc
 clear
 addpath('../github_repo')
 addpath('../IRLS')
+
+% 验证快速计算公式
+a=[1;2;3];
+b=[4;5;6];
+skew_a=skew(a);
+skew_b=skew(b);
+W=magic(3);
+fprintf('expand form\n')
+-1*(-1*trace(b*a')*eye(3)+b*a')*(-1*trace(W)*eye(3)+W')+trace(W'*b*a')*eye(3)-W'*b*a'
+
+
+fprintf('compad form\n')
+skew_a*W*skew_b
+% M=zeros(6,48);
+
+% 验证秩
+% load("A.mat")
+% for i=1:8
+%     M=M+A((i-1)*6+1:i*6,:);
+% end
+
 loadData;
 lambda=5*diag(ones(48,1));
 Simultaneous_linear_solve(graph);
-
-for i=1:8
-    r(i)=rank(A((i-1)*6+1:6*i,:));
-end
