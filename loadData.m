13e7f6149ef4f880f90a2bee97250184a8521346
Timbersaw-wangzw <57893826+Timbersaw-wangzw@users.noreply.github.com>
2022-05-04 19:45:27 +0800

    manifold

--- loadData.m
+++ loadData.m
@@ -3,11 +3,13 @@ addpath(inputdata);
 subdir  = dir( fullfile(inputdata) );
 node_num=1;
 edge_num=1;
+file_list=[];
 for i = 1 : length( subdir )
     if ~isempty(strfind(subdir(i).name,'.txt'))
         if isempty(strfind(subdir(i).name,'filter'))
             graph.node{node_num}.filePath=subdir(i).name;
             graph.node{node_num}.idx=node_num;
+            file_list=[file_list;subdir(i).name];
             node_num=node_num+1;
         end
     end
@@ -20,11 +22,13 @@ for i = 1 : length( subdir )
         temp_target_points=pointCloud(pair_points(4:6,:)');
         [source_points,idx]  = pcdownsample(temp_source_points,'nonuniformGridSample',6);
         target_points=select(temp_target_points,idx);
-        [T,moving,rmse] = pcregistericp(source_points,target_points);
+%         [T,moving,rmse] = pcregistericp(source_points,target_points);
         graph.edge{edge_num}.orgin_pair_points_1 = source_points;
         graph.edge{edge_num}.orgin_pair_points_2 = target_points;
-        graph.edge{edge_num}.relative_pair_points_2 = target_points;
+%         figure(edge_num);
+%         pcshowpair(source_points,target_points);
         if source_points(:,1)==target_points(:,1)
+          
             fprintf('test');
         end
         edge_num=edge_num+1;
@@ -35,33 +39,20 @@ for i=1:node_num-1
 end
 graph=getMultiRegError(graph);
 % 提前获取边信息
-% graph.edge{1}.idx=[1,2];
-% graph.edge{1}.correspondences=[1,2];
-% graph.edge{2}.idx=[1,8];
-% graph.edge{2}.correspondences=[1,2];
-% graph.edge{3}.idx=[2,3];
-% graph.edge{3}.correspondences=[1,2];
-% graph.edge{4}.idx=[2,7];
-% graph.edge{4}.correspondences=[2,7];
-% graph.edge{5}.idx=[2,8];
-% graph.edge{5}.correspondences=[1,2];
-% graph.edge{6}.idx=[3,4];
-% graph.edge{6}.correspondences=[1,2];
-% graph.edge{7}.idx=[3,5];
-% graph.edge{7}.correspondences=[3,5];
-% graph.edge{8}.idx=[3,6];
-% graph.edge{8}.correspondences=[3,5];
-% graph.edge{9}.idx=[3,7];
-% graph.edge{9}.correspondences=[3,5];
-% graph.edge{10}.idx=[4,5];
-% graph.edge{10}.correspondences=[3,5];
-% graph.edge{11}.idx=[4,6];
-% graph.edge{11}.correspondences=[3,5];
-% graph.edge{12}.idx=[5,6];
-% graph.edge{12}.correspondences=[3,5];
-% graph.edge{13}.idx=[6,7];
-% graph.edge{13}.correspondences=[3,5];
-% edge_num=length(graph.edge);
+graph.edge{1}.idx=[1,2];
+graph.edge{2}.idx=[1,8];
+graph.edge{3}.idx=[2,3];
+graph.edge{4}.idx=[2,7];
+graph.edge{5}.idx=[2,8];
+graph.edge{6}.idx=[3,4];
+graph.edge{7}.idx=[3,5];
+graph.edge{8}.idx=[3,6];
+graph.edge{9}.idx=[3,7];
+graph.edge{10}.idx=[4,5];
+graph.edge{11}.idx=[4,6];
+graph.edge{12}.idx=[5,6];
+graph.edge{13}.idx=[6,7];
+edge_num=length(graph.edge);
 
 % idx_p=graph.edge{2}.idx(1);
 % idx_q=graph.edge{2}.idx(2);
@@ -76,16 +67,16 @@ graph=getMultiRegError(graph);
 % plot3(AreaPt1(1,:),AreaPt1(2,:),AreaPt1(3,:),'.','MarkerSize',0.5);
 % plot3(AreaPt2(1,:),AreaPt2(2,:),AreaPt2(3,:),'.','MarkerSize',0.5);
 % [pt1,pt2]=FilterPoints(AreaPt1,AreaPt2);
-% for i=1:edge_num
-%     idx_p=graph.edge{i}.idx(1);
-%     idx_q=graph.edge{i}.idx(2);
-%     AreaPt1=importdata(graph.node(idx_p).filePath);
-%     AreaPt2=importdata(graph.node(idx_q).filePath);
-%     AreaPt1=AreaPt1(:,1:3);
-%     AreaPt2=AreaPt2(:,1:3);
-%
-%     AreaPt1=[AreaPt1,ones(length(AreaPt1(:,1)), 1)]';
-%     AreaPt2=[AreaPt2,ones(length(AreaPt2(:,1)), 1)]';
-%     [pt1,pt2]=FilterPoints(AreaPt1,AreaPt2);
-%     [~,~,~,pair_points]=IRLS_ICP(pt1(1:3,:),pt2(1:3,:),0.0001,'Cauchy',50);
-% end
+for i=1:edge_num
+    idx_p=graph.edge{i}.idx(1);
+    idx_q=graph.edge{i}.idx(2);
+    AreaPt1=importdata(graph.node(idx_p).filePath);
+    AreaPt2=importdata(graph.node(idx_q).filePath);
+    AreaPt1=AreaPt1(:,1:3);
+    AreaPt2=AreaPt2(:,1:3);
+
+    AreaPt1=[AreaPt1,ones(length(AreaPt1(:,1)), 1)]';
+    AreaPt2=[AreaPt2,ones(length(AreaPt2(:,1)), 1)]';
+    [pt1,pt2]=FilterPoints(AreaPt1,AreaPt2);
+    [~,~,~,pair_points]=IRLS_ICP(pt1(1:3,:),pt2(1:3,:),0.0001,'Cauchy',50);
+end
